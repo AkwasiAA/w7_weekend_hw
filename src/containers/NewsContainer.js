5071490a746bc49ba215fb76df1eb178b4dc6643
Akwasi Atakora-Acheampong <aa.acheampong1@gmail.com>
2021-08-01 11:24:15 +0100

    first commit

---
+++ src/containers/NewsContainer.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import NewsList from '../components/NewsList';
+import NewsDetail from '../components/NewsDetail';
+import NewsSelector from '../components/NewsSelect';
+import './NewsContainer.css';
+
+const NewsContainer = () => {
+    const [news, setNews] = useState([]);
+    const [selectedNews, setSelectedNews] = useState(null);
+
+    useEffect(() => {
+        getNews();
+
+    }, [])
+
+    const getNews = function(){
+        fetch('https://content.guardianapis.com/search?q=olympics&format=json&api-key=test')
+        .then(res => res.json())
+        .then(news => setNews(news))
+    }
+
+    const onNewsSelected = function(news){
+        setSelectedNews(news);
+    }
+
+    return (
+        <div className="main-news-container">
+            <NewsSelector news={news} onNewsSelected={onNewsSelected} />
+            {selectedNews ? <NewsDetail selectedNews={selectedNews} /> : null}
+
+        </div>
+    )
+}
+
+export default NewsContainer;
